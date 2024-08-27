4c8ec73ec5e40c32f99768b75e249d5a44bbd6e3
bilal-the-dev <bilalfarooq6001@gmail.com>
2024-08-27 12:44:44 +0500

    s

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,26 +1,9 @@
+const { ActivityType } = require("discord.js");
+
 module.exports = async (client) => {
   console.log(`${client.user.tag} is online.`);
-  /**type */
-  // const ch = client.channels.cache.get("1259860953479122944");
-
-  // const m = await ch.messages.fetch("1269379694760034365");
-  // await m.react("👀");
-  // await m.react("🤑");
-  // await m.react("😕");
-  // await m.react("😁");
-  // await m.react("😂");
-  // const n = ["1", "2", "3"];
-  // let i = 0;
-  // setInterval(async () => {
-  //   try {
-  //     console.log(i);
-
-  //     const c = await ch.edit({ name: n[i] });
-  //     console.log(c);
-
-  //     i++;
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }, 1000 * 10);
+  client.user.setActivity({
+    name: "Sometimes, Kismet happens...me! 🙂",
+    type: ActivityType.Custom,
+  });
 };
